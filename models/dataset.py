671860cd93601c3b630ec14394f8c32e07d1d151
AndrasFajkusz <58299222+faja1234@users.noreply.github.com>
2024-05-23 23:42:53 +0200

    evaluation

---
+++ models/dataset.py
@@ -0,0 +1,23 @@
+import torch.utils.data as data
+class LCDataset(data.Dataset):
+
+    def __init__(self, data, label):
+        """
+        Inputs:
+            size - Number of data points we want to generate
+            std - Standard deviation of the noise (see generate_continuous_xor function)
+        """
+        super().__init__()
+        self.data = data
+        self.label = label
+
+    def __len__(self):
+        # Number of data point we have. Alternatively self.data.shape[0], or self.label.shape[0]
+        return self.data.size(0)
+
+    def __getitem__(self, idx):
+        # Return the idx-th data point of the dataset
+        # If we have multiple things to return (data point and label), we can return them as tuple
+        data_point = self.data[idx]
+        data_label = self.label[idx]
+        return data_point, data_label
--- models/model.py
+++ models/model.py
@@ -1,5 +1,5 @@
 from torch import nn
-class LogisticRegression (nn.Module):
+class Logistic_Regression (nn.Module):
     def __init__(self, num_features):
         super().__init__()
         self.layers = nn.Sequential(
@@ -10,3 +10,16 @@ class LogisticRegression (nn.Module):
     def forward(self, x):
         x = self.layers(x)
         return x
+
+class MLP(nn.Module):
+    def __init__(self, num_features):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(num_features, 3),
+            nn.ReLU(),
+            nn.Linear(3, 1),
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+        out = self.mlp(x)
+        return out
